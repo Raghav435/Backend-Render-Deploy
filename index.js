a3afe8b0e8b4743db12a32ec1321bd0f8d35083a
Raghav Shukla <shuklaraghav435@gmail.com>
2022-11-04 12:08:06 +0530

    added about page

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
+app.get("/about", (req, res)=>{
+    res.send("Welcome to about page")
+})
+
 app.listen(PORT, async () => {
   console.log(`Listening on ${PORT}`);
 });
